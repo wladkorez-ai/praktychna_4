7ef862e08bd1ae6db276515c525a0e254f50ef30
Vlad <wladkorez@gmail.com>
2026-05-06 12:38:09 +0300

    кінцевий цикл

--- praktychna_4.py
+++ praktychna_4.py
@@ -4,3 +4,8 @@ n = int(input("Введіть кількість членів (n): "))
 
 current_b = b1
 
+print(f"Перші {n} члени геометричної прогресії:")
+for i in range(n):
+     print(current_b, end=" ")
+     current_b *= q
+
